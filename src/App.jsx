0bd84c2c951b8ba97f2407a2589ae5dabc92287f
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-04 21:59:59 +0530

    Add About section, Services section, and My Latest Work section to the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 // import React from 'react'
 
+import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
+import MyWork from "./components/MyWork/MyWork";
 import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
+      <Services />
+      <MyWork />
     </div>
   );
 };
