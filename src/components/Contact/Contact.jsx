7aa4056d1792a3fb0a0f8026be9398b8e631c4f0
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-05 12:16:18 +0530

    Integrate Web3Forms to send contact form data via email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 import theme_pattern from "../../assets/theme_pattern.svg";
 import mail_icon from "../../assets/mail_icon.svg";
@@ -6,6 +6,33 @@ import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
 
 const Contact = () => {
+  const [sendEmail, setSendEmail] = useState(false);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "62480016-caa2-4037-839d-6aa386bf1e64");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      // alert(res.message);
+      setSendEmail(true);
+    }
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="contact-title">
@@ -35,7 +62,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <form className="contact-right">
+        <form onSubmit={onSubmit} className="contact-right">
           <label htmlFor="">Your Name</label>
           <input type="text" placeholder="Enter your name" name="name" />
           <label htmlFor="">User Email</label>
@@ -49,6 +76,9 @@ const Contact = () => {
           <button type="submit" className="contact-submit">
             Submit now
           </button>
+          <div className="email-notfication-container">
+            {sendEmail ? <div>Email sent successfully!</div> : <div></div>}
+          </div>
         </form>
       </div>
     </div>
