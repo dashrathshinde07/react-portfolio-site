979bc5b01f459b2a26c3156ce820136c08b4833e
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-05 11:41:41 +0530

    Add smooth scroll functionality using react-anchor-link-smooth-scroll

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Hero.css";
 import profiel_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profiel_img} alt="profileImage" />
       <h1>
         <span>I'm Alex Bennett,</span> frontend developer based in USA.
@@ -14,7 +15,11 @@ const Hero = () => {
         experience in multiple companies like Microsoft, Tesla and Apple.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
