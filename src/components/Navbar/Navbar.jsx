3c04fac4799a71830c92f142b5da68b997ebd3e6
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-05 11:22:30 +0530

    Add Contact section and Footer section to the portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import './Navbar.css'
-import logo from "../../assets/logo.svg"
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.svg";
+import underline from "../../assets/nav_underline.svg";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
   return (
     <div>
-      <div className='navbar'>
-            <img src={logo} alt="logo" />
-            <ul className="nav-menu">
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-            </ul>
-            <div className="nav-connect">Connect With Me</div>
+      <div className="navbar">
+        <img src={logo} alt="logo" />
+        <ul className="nav-menu">
+          <li>
+            <p onClick={() => setMenu("home")}>Home</p>{" "}
+            {menu === "home" ? <img src={underline} alt="" /> : <></>}
+          </li>
+          <li>
+            <p onClick={() => setMenu("about")}>About</p>{" "}
+            {menu === "about" ? <img src={underline} alt="" /> : <></>}
+          </li>
+          <li>
+            <p onClick={() => setMenu("services")}>Services</p>
+            {menu === "services" ? <img src={underline} alt="" /> : <></>}
+          </li>
+          <li>
+            <p onClick={() => setMenu("work")}>Portfolio</p>
+            {menu === "work" ? <img src={underline} alt="" /> : <></>}
+          </li>
+          <li>
+            <p onClick={() => setMenu("contact")}>Contact</p>
+            {menu === "contact" ? <img src={underline} alt="" /> : <></>}
+          </li>
+        </ul>
+        <div className="nav-connect">Connect With Me</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
